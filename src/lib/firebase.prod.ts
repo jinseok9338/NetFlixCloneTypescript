c37923afe7b7de20cc38f9ed9506fde9408e0acb
jinseok9338 <jinseok9338@gmail.com>
2021-09-09 13:40:53 +0000

    Need to change netflix too

--- src/lib/firebase.prod.ts
+++ src/lib/firebase.prod.ts
@@ -1,7 +1,7 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { firebaseConfig } from './firebaseConfig';
+import  firebaseConfig  from './firebaseConfig';
 // import { seedDatabase } from 'src/seed';
 
 // 1) when seeding the database you'll have to uncomment this!
